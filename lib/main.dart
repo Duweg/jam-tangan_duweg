e10c8a7e2d28bf53fea2e6b912d22d6d2f693978
duweg <duwegjayasentana1234@gmail.com>
2020-05-18 21:58:51 +0800

    jam tangan_duweg

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:jamtangan/ui/berandaadmin.dart'; 
+import 'package:jamtangan/ui/berandauser.dart'; 
+import 'package:jamtangan/ui/home.dart'; 
+import 'package:jamtangan/ui/login.dart'; 
+import 'package:flutter/material.dart'; 
+void main() => runApp(   
+  new MaterialApp(     
+    title: 'Penjualan Kacamata',     
+    home: Berandauser(),     
+    routes: <String, WidgetBuilder>{         
+      '/Berandauser': (BuildContext context) => new Berandauser(),         
+      '/Berandaadmin': (BuildContext context) => new Berandaadmin(),         
+      '/login':(BuildContext context)=>Login(),         
+      '/Penjualan': (BuildContext context) => new Home(),       
+      },   
+    ) 
+  ); 
